518888078597db752914c287a2671d88bde29ce1
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-09-16 11:13:38 -0300

    se apaga la musica

---
+++ Assets/Scripts/Codigoparaapagar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Codigoparaapagar : MonoBehaviour
+{
+    [SerializeField] GameObject input;
+    public InputField codigo;
+    public GameObject parlantedos;
+    public GameObject parlanteuno;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) 
+        {
+
+
+
+            if (codigo.text == "04050607")
+            {
+                Destroy(parlantedos);
+                Destroy(parlanteuno);
+            }
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        input.SetActive(true);
+        }
+
+    void OnTriggerExit()
+    {
+        input.SetActive(false);
+    }
+}
